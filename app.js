b4738684b11314636a2733c5bccfa047c6ec44ec
hansijie <hansijie@xiaoniangao.com>
2021-06-11 09:41:28 +0800

    add Nunjucks

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
+const templating = require('./templating')
 const app = new Koa();
-
+const isProduction = process.env.NODE_ENV === 'production';
 
 // log request URL:
 app.use(async (ctx, next) => {
@@ -10,8 +11,23 @@ app.use(async (ctx, next) => {
     await next();
 });
 
+if (! isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
+
 //由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上
 app.use(bodyParser());
+
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+}));
 
 // add router middleware:
 app.use(controller());
--- controllers/hello.js
+++ controllers/hello.js
@@ -1,8 +1,21 @@
 var fn_hello = async (ctx, next) => {
     var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+    ctx.render('hello.html',  {
+        name: name,
+        fruits: ['Apple', 'Pear', 'Banana'],
+        count: 12000
+    });
 };
 
+var fn_extend = async (ctx, next) => {
+    ctx.render('extend.html',   {
+        header: 'Hello',
+        body: 'bla bla bla...'
+    });
+};
+
+
 module.exports = {
-    'GET /hello/:name': fn_hello
+    'GET /hello/:name': fn_hello,
+    'GET /extend': fn_extend
 };
